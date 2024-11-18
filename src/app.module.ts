0bd707c185759217a290bb81593e8b76cedc5659
Mujtaba Hassan <mujhassan786@gmail.com>
2024-11-18 20:14:37 +0200

    add product & user module in the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './core/users/users.module';
+import { ProductsModule } from './core/products/products.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
