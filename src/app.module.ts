4d3ecde3a42ff95abbca022bce199731e2c5d2b4
Mujtaba Hassan <mujhassan786@gmail.com>
2024-11-19 00:03:17 +0200

    update app module to add database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './core/users/users.module';
 import { ProductsModule } from './core/products/products.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [UsersModule, ProductsModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
