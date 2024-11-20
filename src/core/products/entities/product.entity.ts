c17975da14f11d06a28d26583fd2a106aa983a47
Mujtaba Hassan <mujhassan786@gmail.com>
2024-11-21 00:24:13 +0200

    add user and product entity

--- src/core/products/entities/product.entity.ts
+++ src/core/products/entities/product.entity.ts
@@ -1,1 +1,35 @@
-export class Product {}
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  PrimaryKey,
+  AutoIncrement,
+  CreatedAt,
+  DeletedAt,
+} from 'sequelize-typescript';
+
+@Table({
+  timestamps: true,
+  paranoid: true,
+})
+export class Product extends Model<Product> {
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.INTEGER)
+  id: number;
+
+  @Column(DataType.STRING)
+  name: string;
+
+  @Column(DataType.TEXT)
+  description: string;
+
+  @CreatedAt
+  @Column({ type: DataType.DATE, allowNull: false })
+  createdAt: Date;
+
+  @DeletedAt
+  @Column({ type: DataType.DATE, allowNull: true })
+  deletedAt?: Date;
+}
