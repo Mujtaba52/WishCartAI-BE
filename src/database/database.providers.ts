87b321f6fe7401a858b1526b502f291080298c05
Mujtaba Hassan <mujhassan786@gmail.com>
2024-11-20 20:33:18 +0200

    add database provider file

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from 'sequelize-typescript';
 import { databaseConfig } from './database.config';
 import { SEQUELIZE } from 'src/common/db.env.constants';
+import { User } from 'src/core/users/entities/user.entity';
+import { Product } from 'src/core/products/entities/product.entity';
 
 export const databaseProviders = [
   {
@@ -8,10 +10,10 @@ export const databaseProviders = [
     useFactory: async () => {
       const sequelize = new Sequelize({
         ...databaseConfig[process.env.NODE_ENV || 'local'],
-        models: [],
+        models: [User, Product],
       });
-      
-    //   await sequelize.sync();
+
+      //   await sequelize.sync();
       return sequelize;
     },
   },
